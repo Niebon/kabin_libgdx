0ff79add5f61648ddb0a96f9d8c9bbd304940115
Niebon <42673857+Niebon@users.noreply.github.com>
2020-09-14 20:55:37 +0200

    Began libGDX project.

---
+++ desktop/src/dev/kabin/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package dev.kabin.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import dev.kabin.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
