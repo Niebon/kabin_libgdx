0e035f807e3596bc7f7a11e56a07fe95dcb46967
Niebon <niels.bonten@gmail.com>
2020-09-29 22:04:16 +0200

    Added button. Todo: make button trigger load asset.

--- core/src/dev/kabin/MainGame.java
+++ core/src/dev/kabin/MainGame.java
@@ -4,10 +4,17 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import dev.kabin.entities.Entity;
 import dev.kabin.entities.EntityFactory;
 import dev.kabin.entities.EntityGroupProvider;
 import dev.kabin.global.GlobalData;
+import dev.kabin.ui.DevInterface;
 import dev.kabin.utilities.eventhandlers.KeyEventUtil;
 import dev.kabin.utilities.eventhandlers.MouseEventUtil;
 
@@ -17,11 +24,28 @@ public class MainGame extends ApplicationAdapter {
 
 	float stateTime;
 	SpriteBatch batch;
+	Stage stage;
+	Skin skin;
 
 	@Override
 	public void create() {
+		skin = new Skin(Gdx.files.internal("uiskin.json"));
+		stage = new Stage(new ScreenViewport());
+
+		final TextButton button = new TextButton("Load image asset", skin, "default");
+		button.setWidth(200);
+		button.setHeight(50);
+		button.addListener(new ClickListener() {
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+				DevInterface.loadAsset();
+				return true;
+			}
+		});
+
 
 		Gdx.input.setInputProcessor(GlobalData.getInputProcessor());
+		Gdx.input.setInputProcessor(stage);
 		logger.setLevel(GlobalData.getLogLevel());
 		MouseEventUtil.getInstance().addListener(
 				MouseEventUtil.MouseButton.LEFT, true, () -> {
@@ -34,6 +58,7 @@ public class MainGame extends ApplicationAdapter {
 					}
 				}
 		);
+		stage.addActor(button);
 		batch = new SpriteBatch();
 
 
@@ -43,7 +68,8 @@ public class MainGame extends ApplicationAdapter {
 	public void render () {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // This cryptic line clears the screen.
 		stateTime += Gdx.graphics.getDeltaTime(); // Accumulate elapsed animation time.
-
+		stage.act(stateTime);
+		stage.draw();
 
 		EntityGroupProvider.actionForEachEntityOrderedByGroup(e -> e.render(batch, stateTime));
 
